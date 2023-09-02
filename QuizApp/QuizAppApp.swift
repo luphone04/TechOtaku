f69f97b0701ad7881f832296ad4de44142817354
luphone04 <luphonemaw09@gmail.com>
2023-09-02 10:55:55 +0700

    Preparation

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Lu Maw on 02/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
